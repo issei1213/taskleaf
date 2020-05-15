df1c73cda118ab4019702442af2d3e93eff0778f
lord_0bf38w16110335h@yahoo.co.jp <59830008+issei1213@users.noreply.github.com>
2020-05-15 21:53:00 +0900

    他のユーザーが作成したタスクが表示されないことの確認

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -5,18 +5,28 @@ describe "タスク管理機能", type: :system do
     before do
       user_a = FactoryBot.create(:user, name: "ユーザーA", email: "a@example.com")
       FactoryBot.create(:task, name: "最初のタスク", user: user_a)
+
+      visit login_path
+      fill_in "メールアドレス", with: "b@example.com"
+      fill_in "パスワード", with: "password"
+      click_button "ログインする"
     end
 
     context "ユーザーAがログインしている時" do
-      before do
-        visit login_path
-        fill_in "メールアドレス", with: "a@example.com"
-                fill_in "パスワード", with: "password"
-        click_button "ログインする"
+      # before do
+      #   visit login_path
+      #   fill_in "メールアドレス", with: "a@example.com"
+      #           fill_in "パスワード", with: "password"
+      #   click_button "ログインする"
       end
 
+      context "ユーザーBがログインしている時" do
+        before do
+          FactoryBot.create(:user, name: "ユーザーB", email: "b@example.com")
+        end
+
       it "ユーザーAが作成したタスクが表示される" do
-        expect(page).to have_content "最初のタスク"
+        expect(page).to have_no_content "最初のタスク"
       end
     end
   end
