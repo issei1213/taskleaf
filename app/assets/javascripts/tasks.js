dd5cc5029060a9f942dbb3ca488de4c80b16675b
lord_0bf38w16110335h@yahoo.co.jp <59830008+issei1213@users.noreply.github.com>
2020-05-26 15:57:56 +0900

    AjaxでRailsサーバーと通信する

--- app/assets/javascripts/tasks.js
+++ app/assets/javascripts/tasks.js
@@ -1,12 +1,22 @@
 document.addEventListener("turbolinks:load", function(){
   document.querySelectorAll("td").forEach(function(td){
     td.addEventListener("mouseover", function(e){
-      debugger
       e.currentTarget.style.backgroundColor = "#eff";
     });
 
     td.addEventListener("mouseout", function(e){
       e.currentTarget.style.backgroundColor = "";
+    });
+  });
+});
+
+document.addEventListener("turbolinks:load", function(){
+  document.querySelectorAll(".delete").forEach(function(a){
+    a.addEventListener("ajax:success", function(){
+      console.log("デバッグ")
+      var td = a.parentNode;
+      var tr = td.parentNode;
+      tr.style.display = "none";
     });
   });
 });
